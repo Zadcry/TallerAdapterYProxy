20f2b12e39d12243d1cfa3c2c52c9395a20e0c50
Zadcry <zadcry@gmail.com>
2024-08-21 14:40:49 -0500

    Adaptador de Audio Implementado

--- src/main/java/edu/unisabana/dyas/patterns/utils/AdvancedAudioPlayerAdapter.java
+++ src/main/java/edu/unisabana/dyas/patterns/utils/AdvancedAudioPlayerAdapter.java
@@ -1,2 +1,23 @@
-package edu.unisabana.dyas.patterns.utils;public class AdvancedAudioPlayerAdapter {
+package edu.unisabana.dyas.patterns.utils;
+
+public class AdvancedAudioPlayerAdapter implements AudioPlayer{
+
+    private AdvancedAudioPlayer advancedAudioPlayer;
+
+    public AdvancedAudioPlayerAdapter(AdvancedAudioPlayer advancedAudioPlayer) {
+        this.advancedAudioPlayer = advancedAudioPlayer;
+    }
+    @Override
+    public void play(String audioType, String fileName) {
+        if (audioType.equalsIgnoreCase("mp4")) {
+            advancedAudioPlayer.playMp4(fileName);
+        } else if (audioType.equalsIgnoreCase("vlc")) {
+            advancedAudioPlayer.playVlc(fileName);
+        }
+    }
+
+    @Override
+    public void stop() {
+        advancedAudioPlayer.stop();
+    }
 }
